1cb1de97f12f75198fe0368dadd0fd8d977a284e
Dustin Bragg <dustin.bragg@netapp.com>
2018-10-15 11:25:19 -0500

    Less verbose log output in collector

--- collector/docker-entrypoint.sh
+++ collector/docker-entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-python collector.py --proxySocketAddress ${PROXY_ADDRESS} --intervalTime ${COLLECTION_INTERVAL} -i -s -b
+python collector.py --proxySocketAddress ${PROXY_ADDRESS} --intervalTime ${COLLECTION_INTERVAL} -i -s
