a8b1da57c59acb7f6f82b9e605681bb32f1cff7f
Dustin Bragg <dustin.bragg@netapp.com>
2018-10-23 09:29:17 -0500

    Fixed typo

--- collector/docker-entrypoint.sh
+++ collector/docker-entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-if [[ -z "${USERNAME}" ]] || [[ -z "${PASSWORD}" ]]; then # potential environment vars from Dockerfile
+if [[ -z "${USERNAME}" ]] || [[ -z "${PASSWORD}" ]]; then # potential environment vars from Docker compose file
     python collector.py --proxySocketAddress ${PROXY_ADDRESS} --intervalTime ${COLLECTION_INTERVAL} -i -s
 else
     python collector.py -u ${USERNAME} -p ${PASSWORD} --proxySocketAddress ${PROXY_ADDRESS} --intervalTime ${COLLECTION_INTERVAL} -i -s
