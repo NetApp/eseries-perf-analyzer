9dec0470e96c292f0ddb60f11de6829fa16d3a0c
Dustin Bragg <Dustin.Bragg.netapp.com>
2019-08-01 16:38:57 -0400

    Created migration script for migrating disk and volume statistics from previous Graphite instance to InfluxDB
    Modified collection script and system dashboard to better reflect legacy data

--- scripts/migrate.sh
+++ scripts/migrate.sh
@@ -1,21 +1,15 @@
 #!/bin/sh
 
 ##
-# Configuration
+## Configuration
 ##
 HOST_NAME="localhost"
-PROXY_USERNAME="admin"
-PROXY_PASSWORD="admin"
-GRAFANA_USERNAME="admin"
-GRAFANA_PASSWORD="admin"
+GRAFANA_USER="admin"
+GRAFANA_PASS="admin"
 
-
-##
-# Execution
-##
 # install dependencies
 pip install requests
 pip install influxdb
 rm -rf /root/.cache
 # run migration script
-python /home/scripts/migrate.py -n ${HOST_NAME} -u ${GRAFANA_USERNAME} -p ${GRAFANA_PASSWORD} -w ${PROXY_USERNAME} -a ${PROXY_PASSWORD}
+python /home/scripts/migrate.py -n ${HOST_NAME} -u ${GRAFANA_USER} -p ${GRAFANA_PASS}
