bccbd9296a86c28ccd3c995095432d4813d05439
Dustin Bragg <Dustin.Bragg@netapp.com>
2019-08-06 18:01:57 -0400

    Migration now properly includes system and volume ids

--- scripts/migrate.sh
+++ scripts/migrate.sh
@@ -1,15 +1,21 @@
 #!/bin/sh
 
 ##
-## Configuration
+# Configuration
 ##
 HOST_NAME="localhost"
-GRAFANA_USER="admin"
-GRAFANA_PASS="admin"
+PROXY_USERNAME="admin"
+PROXY_PASSWORD="admin"
+GRAFANA_USERNAME="admin"
+GRAFANA_PASSWORD="admin"
 
+
+##
+# Execution
+##
 # install dependencies
 pip install requests
 pip install influxdb
 rm -rf /root/.cache
 # run migration script
-python /home/scripts/migrate.py -n ${HOST_NAME} -u ${GRAFANA_USER} -p ${GRAFANA_PASS}
+python /home/scripts/migrate.py -n ${HOST_NAME} -u ${GRAFANA_USERNAME} -p ${GRAFANA_PASSWORD} -w ${PROXY_USERNAME} -a ${PROXY_PASSWORD}
