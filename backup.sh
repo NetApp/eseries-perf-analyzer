3c2ee1edf80325241cb5a8c70bb8d6daac6b94f1
Dustin Bragg <Dustin.Bragg@netapp.com>
2019-10-21 17:54:23 -0400

    Inital commit
    Removed all ESeries-specific components

--- backup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-docker run --rm --name tempbak --network "host" -v `pwd`/ansible/dashboards/backup:/home/dashboards/backup ansible backup.yml
-echo -e "******* [ Dashboards backup made in the \"./ansible/dashboards/backup/\" directory ] *******\n"
--- collector/tests/initiate_testing.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker run --rm -v $PWD/collector:/home/collector $1/python-base:$2 /bin/sh -c "chmod +x /home/collector/tests/runtests.sh;/home/collector/tests/runtests.sh"
