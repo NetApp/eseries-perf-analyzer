f3918d1d5c38708ff7c7ba3472e1a48faad3d73b
Dustin Bragg <Dustin.Bragg@netapp.com>
2019-10-22 13:28:17 -0400

    Fixed build issue related to image and project names

--- scripts/plugin_build_info.sh
+++ scripts/plugin_build_info.sh
@@ -22,9 +22,9 @@ do
     do
 	arr=($line)
 	if [ "$plugins_build_data" = "" ]; then
-	    plugins_build_data="echo \"[PLUGINS] Building '${plugin_name}' component: ${arr[0]}\"; docker build -t ${PROJ_NAME}-plugin/${plugin_name}/${arr[0]} ${plugin_dir}/${arr[1]}"    
+	    plugins_build_data="echo \"[PLUGINS] Building '${plugin_name}' component: ${arr[0]}\"; docker build --build-arg PROJ_NAME=${PROJ_NAME} -t ${PROJ_NAME}-plugin/${plugin_name}/${arr[0]} ${plugin_dir}/${arr[1]}"    
 	    else
-	        plugins_build_data="$plugins_build_data; echo \"[PLUGINS] Building '${plugin_name}' component: ${arr[0]}\"; docker build -t ${PROJ_NAME}-plugin/${plugin_name}/${arr[0]} ${plugin_dir}/${arr[1]}"
+	        plugins_build_data="$plugins_build_data; echo \"[PLUGINS] Building '${plugin_name}' component: ${arr[0]}\"; docker build --build-arg PROJ_NAME=${PROJ_NAME} -t ${PROJ_NAME}-plugin/${plugin_name}/${arr[0]} ${plugin_dir}/${arr[1]}"
 		fi
 	
     done < $file
