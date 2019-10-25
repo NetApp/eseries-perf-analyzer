c35a55d7c3e085174a15aea58eb201505421b8b6
Dustin Bragg <Dustin.Bragg@netapp.com>
2019-10-25 16:19:43 -0400

    Added better output distinguishing when individual plugins are started/stopped

--- scripts/plugin_compose_info.sh
+++ scripts/plugin_compose_info.sh
@@ -7,10 +7,13 @@ plugins_compose_files=$(find plugins/ -type f -name "docker-compose.yml")
 plugins_compose_data=""
 for file in $plugins_compose_files
 do
+    # extract this plugin's directory name from the docker-compose path
+    plugin_name=$(echo $file | grep -o "/.*/" | cut -d "/" -f 2)
+
     if [ "$plugins_compose_data" = "" ]; then
-        plugins_compose_data="docker-compose -f ${file} $1"
+        plugins_compose_data="echo \"[PLUGINS] '${plugin_name}'\"; docker-compose -f ${file} $1"
     else
-        plugins_compose_data="${plugins_compose_data}; docker-compose -f ${file} $1"
+        plugins_compose_data="${plugins_compose_data}; echo \"[PLUGINS] '${plugin_name}'\"; docker-compose -f ${file} $1"
     fi
 done
 
